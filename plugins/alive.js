b6e8a2129a022ec00ddbce0a464c802cae56b514
Alosious Benny <105029178+Dot-ser@users.noreply.github.com>
2024-07-30 18:28:49 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -135,7 +135,7 @@ Module({
   const start = new Date().getTime()
   let sent_msg = await message.sendReply('*❮ ᴛᴇsᴛɪɴɢ ᴘɪɴɢ ❯*')
   const end = new Date().getTime()
-  await message.edit('*ʟᴀᴛᴇɴᴄʏ: ' + (end - start) + ' _ᴍs_*',message.jid,sent_msg.key)
+  await message.edit('*ʟᴀᴛᴇɴᴄʏ💀: ' + (end - start) + ' _ᴍs_*',message.jid,sent_msg.key)
 }));
 Module({
   pattern: 'uptime',
